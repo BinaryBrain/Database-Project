5729af7ca486bab6d886ec23cf87474c3976074c
Sacha Bron <sacha.bron@copperflake.com>
2014-04-24 13:32:11 +0200

    Fix SQL types

--- create_tables.sql
+++ create_tables.sql
@@ -1,65 +1,63 @@
-
 CREATE TABLE Area(
-ID_Area CHAR(60),
-Name CHAR(60),
-Type CHAR(60),
+ID_Area INT,
+Name VARCHAR2(1000),
+Type VARCHAR2(60),
 PRIMARY KEY(ID_Area)
 );
 
 CREATE TABLE Genre
-(ID_Genre CHAR(60),
-Name CHAR(60),
-Count CHAR(60),
+(ID_Genre INT,
+Name VARCHAR2(1000),
+Count INT,
 PRIMARY KEY(ID_Genre));
 
 CREATE TABLE Recording
-(ID_Recording CHAR(60),
-Name CHAR(60),
-Length CHAR(60),
+(ID_Recording INT,
+Name VARCHAR2(1000),
+Length INT,
 PRIMARY KEY(ID_Recording));
 
 CREATE TABLE Release
-(ID_Release CHAR(60),
-Name CHAR(60),
+(ID_Release INT,
+Name VARCHAR2(1000),
 PRIMARY KEY(ID_Release));
 
 CREATE TABLE Artist
-(ID_Artist CHAR(60),
-Name CHAR(60),
-Type CHAR(60),
-Gender CHAR(60),
-ID_Area CHAR(60),
-PRIMARY KEY(ID_Artist),
-FOREIGN KEY(ID_Area) REFERENCES Area
+(ID_Artist INT,
+Name VARCHAR2(1000),
+Type VARCHAR2(60),
+Gender VARCHAR2(20),
+ID_Area INT,
+PRIMARY KEY(ID_Artist)
 );
 
 CREATE TABLE Medium
-(ID_Medium CHAR(60),
-Format CHAR(60),
-ID_Release CHAR(60),
-PRIMARY KEY(ID_Medium),
-FOREIGN KEY(ID_Release) REFERENCES Release);
+(ID_Medium INT,
+Format VARCHAR2(60),
+ID_Release INT,
+PRIMARY KEY(ID_Medium)
+);
 
 CREATE TABLE Track
-(ID_Track CHAR(60),
-Position CHAR(60),
-ID_Medium CHAR(60),
-ID_Recording CHAR(60),
+(ID_Track INT,
+Position INT,
+ID_Medium INT,
+ID_Recording INT,
 PRIMARY KEY(ID_Track),
 FOREIGN KEY(ID_Medium) REFERENCES Medium,
 FOREIGN KEY(ID_Recording) REFERENCES Recording
 );
 
 CREATE TABLE Artist_Genre
-(ID_Artist CHAR(60),
-ID_Genre CHAR(60),
+(ID_Artist INT,
+ID_Genre INT,
 PRIMARY KEY(ID_Artist, ID_Genre),
 FOREIGN KEY(ID_Artist) REFERENCES Artist,
 FOREIGN KEY(ID_Genre) REFERENCES Genre);
 
 CREATE TABLE Artist_Track
-(ID_Artist CHAR(60),
-ID_Track CHAR(60),
+(ID_Artist INT,
+ID_Track INT,
 PRIMARY KEY(ID_Artist, ID_Track),
 FOREIGN KEY(ID_Artist) REFERENCES Artist,
 FOREIGN KEY(ID_Track) REFERENCES Track);
