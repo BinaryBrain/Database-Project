2a9a43b9cd60ed5d065cbec8e70f7d77068cb56e
Sacha Bron <sacha.bron@copperflake.com>
2014-05-06 12:02:05 +0200

    fix search bug

--- client/view/scripts/ui.js
+++ client/view/scripts/ui.js
@@ -18,15 +18,15 @@
 		},
 		{
 			name: "e) Female artist with the most genres",
-			request: "SELECT NAME FROM (SELECT A.NAME AS NAME, COUNT(DISTINCT G.ID_GENRE) AS COUNT_GENRE FROM ARTIST_GENRE G, ARTIST A WHERE A.ID_ARTIST= G.ID_ARTIST AND A.GENDER='Female' GROUP BY A.NAME ORDER BY COUNT_GENRE DESC) WHERE ROWNUM=1"
+			request: "SELECT NAME FROM (SELECT A.NAME AS NAME, COUNT(G.ID_GENRE) AS COUNT_GENRE FROM ARTIST_GENRE G, ARTIST A WHERE A.ID_ARTIST= G.ID_ARTIST AND A.GENDER='Female' GROUP BY A.NAME ORDER BY COUNT_GENRE DESC) WHERE ROWNUM=1"
 		},
 		{
 			name: "f) ?Cities that have more female artist than male artist",
 			request: "--SELECT B.name FROM Area B WHERE B.type='City' AND (SELECT Count(*) FROM Artist A WHERE A.gender='Female' AND A.ID_AREA= B.ID_AREA) > (SELECT Count(*) FROM Artist A1 WHERE A1.gender='Male' AND A1.ID_AREA=B.ID_AREA)"
 		},
 		{
-			name: "g) The release with the most number of tracks",
-			request: "SELECT NAME FROM (SELECT R.name as name, COUNT(DISTINCT T.ID_TRACK) AS COUNT_TRACK FROM RELEASE R, TRACK T, MEDIUM M WHERE R.ID_RELEASE = M.ID_RELEASE AND M.ID_MEDIUM = T.ID_MEDIUM GROUP BY R.NAME ORDER BY COUNT_TRACK DESC) WHERE ROWNUM=1"
+			name: "g) The releases with the most number of tracks",
+			request: "SELECT R.name as name, COUNT(T.ID_TRACK) AS COUNT_TRACK FROM RELEASE R, TRACK T, MEDIUM M WHERE R.ID_RELEASE = M.ID_RELEASE AND M.ID_MEDIUM = T.ID_MEDIUM GROUP BY R.NAME ORDER BY COUNT_TRACK DESC"
 		}
 	]
 
@@ -140,13 +140,13 @@
 
 		destroyTabs()
 
-		var tables, requests
+		var tables
+		var requests = []
 		if(table === 'artist') {
 			tables = ['area', 'genre', 'release']
-			requests = []
 			requests['area'] = "SELECT area.NAME, area.ID_AREA, area.TYPE FROM area, artist WHERE area.ID_AREA=artist.ID_AREA AND artist."+idCol+"='"+id+"'"
 			requests['genre'] = "SELECT g.NAME, g.ID_GENRE, g.COUNT FROM artist_genre x, genre g WHERE g.ID_GENRE = x.ID_GENRE AND x."+idCol+"='"+id+"'"
-			requests['release'] = "SELECT r.NAME, r.ID_RELEASE FROM artist a, release r, medium m, track t, artist_track s WHERE s.ID_ARTIST = a.ID_ARTIST AND s.ID_TRACK = t.ID_TRACK AND t.ID_MEDIUM = m.ID_MEDIUM AND m.ID_RELEASE = r.ID_RELEASE AND a."+idCol+"='"+id+"'"
+			requests['release'] = "SELECT DISTINCT r.NAME, r.ID_RELEASE FROM artist a, release r, medium m, track t, artist_track s WHERE s.ID_ARTIST = a.ID_ARTIST AND s.ID_TRACK = t.ID_TRACK AND t.ID_MEDIUM = m.ID_MEDIUM AND m.ID_RELEASE = r.ID_RELEASE AND a."+idCol+"='"+id+"'"
 		} else if (table === 'area') {
 			tables = ['artist']
 			requests['artist'] = "SELECT a.NAME, a.ID_ARTIST, a.GENDER, a.ID_AREA, a.TYPE FROM artist a WHERE a.ID_AREA = "+id
@@ -156,7 +156,7 @@
 		}  else if (table === 'recording') {
 			tables = ['artist', 'release']
 			requests['artist'] = "SELECT a.NAME, a.ID_ARTIST, a.GENDER, a.ID_AREA, a.TYPE FROM artist a, track t, artist_track x WHERE x.ID_TRACK = t.ID_TRACK AND x.ID_ARTIST = a.ID_ARTIST AND t.ID_RECORDING = "+id
-			requests['release'] = "SELECT r.NAME, r.ID_RELEASE FROM track t, medium m, release r WHERE t.ID_MEDIUM = m.ID_MEDIUM AND m.ID_RELEASE = r.ID_RELEASE AND t.ID_RECORDING = "+id
+			requests['release'] = "SELECT DISTINCT r.NAME, r.ID_RELEASE FROM track t, medium m, release r WHERE t.ID_MEDIUM = m.ID_MEDIUM AND m.ID_RELEASE = r.ID_RELEASE AND t.ID_RECORDING = "+id
 		}  else if (table === 'relase') {
 			tables = ['artist', 'recording']
 			requests['artist'] = "SELECT a.NAME, a.ID_ARTIST, a.GENDER, a.ID_AREA, a.TYPE FROM artist a, medium m, track t, artist_track x WHERE t.ID_MEDIUM = m.ID_MEDIUM AND x.ID_TRACK = t.ID_TRACK AND x.ID_ARTIST = a.ID_ARTIST AND m.ID_RELEASE = "+id
