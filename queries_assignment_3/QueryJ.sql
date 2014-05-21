d6ebf5e35727525bdc25f4e820f803901132913f
Elieva <elieva.pignat@epfl.ch>
2014-05-21 12:46:34 +0200

    change query H add query J
    query h does not work correctly
    Query J works!

---
+++ queries_assignment_3/QueryJ.sql
@@ -0,0 +1,17 @@
+--For each of the 10 genres with the most artists, list the female artist that has recorded the highest 
+--number of tracks. 
+SELECT NAME
+FROM(
+  SELECT AG.ID_GENRE, A.NAME AS NAME, COUNT(DISTINCT AT.ID_TRACK), ROW_NUMBER()OVER (PARTITION BY AG.ID_GENRE ORDER BY COUNT(DISTINCT AT.ID_TRACK) DESC) AS RN
+  FROM DUMMY_ARTIST A, DUMMY_ARTIST_TRACK AT, DUMMY_ARTIST_GENRE AG
+  WHERE A.ID_ARTIST= AT.ID_ARTIST  AND A.GENDER='Female' AND A.ID_ARTIST=AG.ID_ARTIST AND AG.ID_GENRE IN (SELECT *
+                                                                                                          FROM(
+                                                                                                          SELECT G.ID_GENRE
+                                                                                                          FROM DUMMY_ARTIST_GENRE G
+                                                                                                          GROUP BY G.ID_GENRE
+                                                                                                          ORDER BY COUNT(DISTINCT G.ID_ARTIST) DESC)
+                                                                                                          WHERE ROWNUM <=2)
+  GROUP BY AG.ID_GENRE, A.ID_ARTIST, A.NAME
+  )
+WHERE RN=1;
+
--- queries_assignment_3/queryH.sql
+++ queries_assignment_3/queryH.sql
@@ -42,3 +42,46 @@ SELECT AR.ID_AREA AS AREA,
          ORDER BY COUNT(DISTINCT AT.ID_TRACK) DESC)
     WHERE ROWNUM = 1) AS MALE_TRACK
 FROM DUMMY_AREA AR;
+SELECT NAME_F AS AREA, ANAME_F AS FEMALE_ARTIST,ANAME_M AS MALE_ARTIST, ANAME_G AS GROUP_ARTIST
+FROM(
+SELECT NAME_F, ANAME_F
+FROM(
+SELECT AR.NAME AS NAME_F, A.NAME AS ANAME_F, ROW_NUMBER()OVER(PARTITION BY AR.ID_AREA, AR.NAME ORDER BY COUNT(DISTINCT A.ID_ARTIST)) AS RN 
+FROM DUMMY_ARTIST A, DUMMY_AREA AR, DUMMY_ARTIST_TRACK AT
+WHERE A.GENDER='Female' AND A.ID_AREA=AR.ID_AREA AND A.ID_ARTIST=AT.ID_ARTIST AND A.ID_AREA IN (SELECT AR.ID_AREA
+                                                                                                FROM DUMMY_ARTIST AR
+                                                                                                GROUP BY AR.ID_AREA
+                                                                                               HAVING COUNT(DISTINCT AR.ID_ARTIST)>1
+                                                                                                  )
+GROUP BY AR.ID_AREA, AR.NAME, A.ID_ARTIST, A.NAME
+ORDER BY COUNT(DISTINCT A.ID_ARTIST)
+)
+WHERE RN=1), 
+(SELECT NAME_M, ANAME_M
+FROM(
+SELECT AR.NAME AS NAME_M, A.NAME AS ANAME_M, ROW_NUMBER()OVER(PARTITION BY AR.ID_AREA, AR.NAME ORDER BY COUNT(DISTINCT A.ID_ARTIST)) AS RN 
+FROM DUMMY_ARTIST A, DUMMY_AREA AR, DUMMY_ARTIST_TRACK AT
+WHERE A.GENDER='Male' AND A.ID_AREA=AR.ID_AREA AND A.ID_ARTIST=AT.ID_ARTIST AND A.ID_AREA IN (SELECT AR.ID_AREA
+                                                                                                FROM DUMMY_ARTIST AR
+                                                                                                GROUP BY AR.ID_AREA
+                                                                                               HAVING COUNT(DISTINCT AR.ID_ARTIST)>1
+                                                                                                  )
+GROUP BY AR.ID_AREA, AR.NAME, A.ID_ARTIST, A.NAME
+ORDER BY COUNT(DISTINCT A.ID_ARTIST)
+)
+WHERE RN=1), (SELECT NAME_G, ANAME_G
+FROM(
+SELECT AR.NAME AS NAME_G, A.NAME AS ANAME_G, ROW_NUMBER()OVER(PARTITION BY AR.ID_AREA, AR.NAME ORDER BY COUNT(DISTINCT A.ID_ARTIST)) AS RN 
+FROM DUMMY_ARTIST A, DUMMY_AREA AR, DUMMY_ARTIST_TRACK AT
+WHERE A.TYPE='Group' AND A.ID_AREA=AR.ID_AREA AND A.ID_ARTIST=AT.ID_ARTIST AND A.ID_AREA IN (SELECT AR.ID_AREA
+                                                                                                FROM DUMMY_ARTIST AR
+                                                                                                GROUP BY AR.ID_AREA
+                                                                                               HAVING COUNT(DISTINCT AR.ID_ARTIST)>1
+                                                                                                  )
+GROUP BY AR.ID_AREA, AR.NAME, A.ID_ARTIST, A.NAME
+ORDER BY COUNT(DISTINCT A.ID_ARTIST)
+)
+WHERE RN=1) GR
+WHERE NAME_F=NAME_M AND NAME_G=NAME_F;
+
+
