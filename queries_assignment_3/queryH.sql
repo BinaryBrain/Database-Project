f044939b731df7befa71c1b72bc00e4212fbe31a
Elieva <elieva.pignat@epfl.ch>
2014-06-02 12:32:08 +0200

    modif query +report

--- queries_assignment_3/queryH.sql
+++ queries_assignment_3/queryH.sql
@@ -37,7 +37,7 @@ WHERE AM.GENDER='Female' AND AM.ID_ARTIST=ATM.ID_ARTIST AND AM.ID_AREA IN(SELECT
                                                                          FROM AREA AR2, ARTIST A
                                                                           WHERE A.ID_AREA= AR2.ID_AREA
                                                                           GROUP BY AR2.ID_AREA
-                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=3)
+                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=30)
 GROUP BY AM.ID_AREA,  AM.ID_ARTIST, AM.NAME
 )WHERE RN<=1) F,
 (SELECT AREA_ID, ART_NAME
@@ -48,7 +48,44 @@ WHERE AM.TYPE='Group' AND AM.ID_ARTIST=ATM.ID_ARTIST AND AM.ID_AREA IN(SELECT AR
                                                                          FROM AREA AR2, ARTIST A
                                                                           WHERE A.ID_AREA= AR2.ID_AREA
                                                                           GROUP BY AR2.ID_AREA
-                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=3)
+                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=30)
 GROUP BY AM.ID_AREA,  AM.ID_ARTIST, AM.NAME
 )WHERE RN<=1) G
 WHERE A.ID_AREA=M.AREA_ID AND A.ID_AREA=G.AREA_ID AND A.ID_AREA=F.AREA_ID;
+
+
+SELECT AREA_NAME, ART_NAME, ART_TYPE
+FROM(
+SELECT AR.NAME AS AREA_NAME, A.NAME AS ART_NAME, A.GENDER AS ART_TYPE, COUNT(DISTINCT AT.ID_TRACK AS COUNT_TRACK, ROW_NUMBER()OVER (PARTITION BY AM.ID_AREA ORDER BY COUNT(DISTINCT AT.ID_TRACK)DESC) AS RN
+FROM ARTIST A, AREA AR, ARTIST_TRACK AT
+WHERE A.ID_AREA = AR.ID_AREA AND A.GENDER='Male' AND AT.ID_ARTIST=A.ID_ARTIST AND AR.ID_AREA IN(SELECT AR2.ID_AREA
+                                                                         FROM AREA AR2, ARTIST A
+                                                                          WHERE A.ID_AREA= AR2.ID_AREA
+                                                                          GROUP BY AR2.ID_AREA
+                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=30)
+GROUP BY AR.ID_AREA, AM.NAME
+)WHERE RN<=1
+UNION
+SELECT AREA_NAME, ART_NAME, ART_TYPE
+FROM(
+SELECT AR.NAME AS AREA_NAME, A.NAME AS ART_NAME, A.GENDER AS ART_TYPE, COUNT(DISTINCT AT.ID_TRACK AS COUNT_TRACK, ROW_NUMBER()OVER (PARTITION BY AM.ID_AREA ORDER BY COUNT(DISTINCT AT.ID_TRACK)DESC) AS RN
+FROM ARTIST A, AREA AR, ARTIST_TRACK AT
+WHERE A.ID_AREA = AR.ID_AREA AND A.GENDER='Female' AND AT.ID_ARITST=A.ID_ARTIST AND AR.ID_AREA IN(SELECT AR2.ID_AREA
+                                                                         FROM AREA AR2, ARTIST A
+                                                                          WHERE A.ID_AREA= AR2.ID_AREA
+                                                                          GROUP BY AR2.ID_AREA
+                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=30)
+GROUP BY AR.ID_AREA, AM.NAME
+)WHERE RN<=1
+UNION
+SELECT AREA_NAME, ART_NAME, ART_TYPE
+FROM(
+SELECT AR.NAME AS AREA_NAME, A.NAME AS ART_NAME, A.TYPE AS ART_TYPE, COUNT(DISTINCT AT.ID_TRACK AS COUNT_TRACK, ROW_NUMBER()OVER (PARTITION BY AM.ID_AREA ORDER BY COUNT(DISTINCT AT.ID_TRACK)DESC) AS RN
+FROM ARTIST A, AREA AR, ARTIST_TRACK AT
+WHERE A.ID_AREA = AR.ID_AREA AND A.TYPE='Group' AND AT.ID_ARTIST=A.ID_ARTIST AND AR.ID_AREA IN(SELECT AR2.ID_AREA
+                                                                         FROM AREA AR2, ARTIST A
+                                                                          WHERE A.ID_AREA= AR2.ID_AREA
+                                                                          GROUP BY AR2.ID_AREA
+                                                                          HAVING COUNT(DISTINCT A.ID_ARTIST)>=30)
+GROUP BY AR.ID_AREA, AM.NAME
+)WHERE RN<=1
--- queries_assignment_3_real_DB/queryN.sql
+++ queries_assignment_3_real_DB/queryN.sql
@@ -1,41 +1,22 @@
 --List the top 10 releases with the most collaborations, i.e., releases where one artist is performing all 
 --songs and the highest number of different guest artists contribute to the album.
-/* First create the table of releases where one artist perform all the songs. More precisely, we count the number of track 
-the artist did on that release and we count the number of track the release has in total and keep this release if the equality holds. 
-Then we check if for the medium that contains the matching release there exists a different artist that contribute to 
-a same track of this release (we have this information by using the precedent table in the from of this new query)
-Finally we order the releases (that match these two conditions) by their number of artists and select the top 10.
-*/
-/*SELECT *
-FROM(
-      SELECT ALLR, COUNT(DISTINCT AT2.ID_ARTIST)
-      FROM (
-                SELECT A.ID_ARTIST AS ALLA, M.ID_RELEASE AS ALLR, M.ID_MEDIUM AS ALLM, COUNT(DISTINCT T.ID_TRACK), COUNT(DISTINCT T2.ID_TRACK)
-                FROM ARTIST A, ARTIST_TRACK AT, TRACK T, MEDIUM M, MEDIUM M2, TRACK T2
-                WHERE A.ID_ARTIST = AT.ID_ARTIST AND AT.ID_TRACK = T.ID_TRACK AND T.ID_MEDIUM = M.ID_MEDIUM AND M2.ID_MEDIUM = M.ID_MEDIUM AND T2.ID_MEDIUM = M2.ID_MEDIUM
-                GROUP BY A.ID_ARTIST,M.ID_RELEASE, M.ID_MEDIUM
-                HAVING COUNT(DISTINCT T.ID_TRACK) = COUNT(DISTINCT T2.ID_TRACK)), 
-                TRACK T2, ARTIST_TRACK AT2
-      WHERE T2.ID_MEDIUM = ALLM AND AT2.ID_ARTIST <> ALLA AND AT2.ID_TRACK = T2.ID_TRACK
-      GROUP BY ALLR
-      ORDER BY COUNT(DISTINCT AT2.ID_ARTIST) DESC)
-WHERE ROWNUM <= 10;*/
+
 
 SELECT *
 FROM(
       SELECT B.RELEASE_ID, C.COUNT_ART
       FROM
-      --NBRE DE TRACK PAR ARTIST PAR RELEASE
+      --# OF TRACKS PER ARTIST PER RELEASE
       (SELECT M.ID_RELEASE AS RELEASE_ID, COUNT(DISTINCT T.ID_TRACK) AS COUNT_TRACK
       FROM ARTIST A, TRACK T, MEDIUM M, ARTIST_TRACK AT
       WHERE A.ID_ARTIST=AT.ID_ARTIST AND AT.ID_TRACK=T.ID_TRACK AND T.ID_MEDIUM=M.ID_MEDIUM
       GROUP BY M.ID_RELEASE, A.ID_ARTIST) A,
-      --NBRE DE TRACK PAR RELEASE
+      --# OF TRACK PER RELEASE
       (SELECT  M.ID_RELEASE AS RELEASE_ID, COUNT(DISTINCT T.ID_TRACK)AS COUNT_TRACK
       FROM MEDIUM M, TRACK T
       WHERE T.ID_MEDIUM=M.ID_MEDIUM
       GROUP BY M.ID_RELEASE) B,
-      --NBRE D'ARTISTES DIFFERENTS PAR RELEASE
+      --# OF DIFFERENT ARTISTS PER RELEASE
       (SELECT M.ID_RELEASE AS RELEASE_ID,COUNT(DISTINCT A.ID_ARTIST)AS COUNT_ART, COUNT(DISTINCT T.ID_TRACK) AS COUNT_TRACK
       FROM ARTIST A, TRACK T, MEDIUM M, ARTIST_TRACK AT
       WHERE A.ID_ARTIST=AT.ID_ARTIST AND AT.ID_TRACK=T.ID_TRACK AND T.ID_MEDIUM=M.ID_MEDIUM
